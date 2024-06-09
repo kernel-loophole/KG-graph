904e4937345b7b6b9a8c7ab06ca191530d01c4b1
kernel_loophole <p190119@nu.edu.pk>
2024-06-09 15:41:41 +0500

    ner plot remove

--- open_pickle.py
+++ open_pickle.py
@@ -1,12 +1,7 @@
 import pickle
-
-# File path of the pickle file
 file_path = 'data.pkl'
 
-# Open the pickle file in read-binary mode
-with open(file_path, 'rb') as file:
-    # Load the Python object from the pickle file
-    loaded_data = pickle.load(file)
 
-# Now you can use the loaded_data object
+with open(file_path, 'rb') as file:
+    loaded_data = pickle.load(file)
 print(loaded_data)
