bf5979f6e474cc2ac2c054895e53eec8ff820faa
kernel_loophole <p190119@nu.edu.pk>
2024-07-29 10:12:40 -0400

    main function to call ner extr

--- graph_gen/main_ner.py
+++ graph_gen/main_ner.py
@@ -60,9 +60,11 @@ def main_ner():
     # data=rows[1]
     # data=rows[0:2]
     # print(rows[0])
-    sub_list = rows[1:10]
+    sub_list = rows[1:2]
+    test_data=[(1,"In a major relief for incarcerated ex-premier and PTI founder , the Nawaz Sahraif  on Thursday set aside his physical remand in a dozen new cases over last year’s May 9 riots"),(2,"Rumours started circulating that Petitioner No. 1 Imran Khan will be shifted to the custody of Prvez Mushraf today (i.e. 25.7.2024) in connection with the very cases which form the subject matter of Writ Petition No. 45901 of 2024 and connected matters"),(3,"Imran Khan  is a Pakistani politician and former cricketer who served as the 22nd prime minister of Pakistan from August 2018 until April 2022. He is the founder and former chairman of the political party Pakistan Tehreek-e-Insaf from 1996 to 2023"),(4,"The “arrest” in these cases had come just a day after the former prime minister and his wife Bushra Bibi had been rearrested in a new Toshakhana case — following their acquittal in the Iddat case, which had them on the brink of being free from jail.")]
+    print(test_data)
     # print(sub_list)
-    mining_thread = threading.Thread(target=execute_mining, args=(sub_list,))
+    mining_thread = threading.Thread(target=execute_mining, args=(test_data,))
     mining_thread.start()
 
     mining_thread.join()
